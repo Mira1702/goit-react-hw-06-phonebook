0af4d519cfa67fb55b9b20038fca42de1c1985a0
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-03-29 23:17:55 +0300

    не работает алерт и фильтр

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -2,64 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import contactsActions from '../../Redux/Contacts/actions';
 
-const ContactItem = ({ name, number, id, onRemove }) => {  
+const ContactItem = ({ id, name, number, onRemove }) => {  
   return (
-    <li >
+    <li id={id}>
       <span>{name}</span>: <span>{number}</span>
-      <button        
-        type="button"
-        onClick={() => {onRemove(id);
-        }}
-      >
-        Delete
-      </button>
+      <button type="button" id={id} onClick={() => onRemove(id)} >Delete</button>
     </li>
   );
 };
 
-function Contacts ({ contacts, filter }) {
-  const filterContacts = (contacts) => {
-    const normalizedFilter = filter.toLowerCase();
-    
-      return contacts.filter(contact => {
-        return contact.name.toLowerCase().includes(normalizedFilter);
-      });
-    
-  };
-  console.log(contacts)
+const Contacts = ({ contacts, onRemove } ) => {  
+  
   return (
     <ul>
-      {filterContacts(contacts).map((contact) => (
-        <ContactItem
-          key={contact.id}
-          id={contact.id}
-          name={contact.name}
-          number={contact.number}
-        />
+      {contacts.map((contact) => (        
+          <ContactItem
+            key={contact.id}
+            id={contact.id}
+            name={contact.name}
+            number={contact.number}
+            onRemove={onRemove}
+          />                  
       ))}
     </ul>
   );
 }
 
-// const Contacts = ({ contacts, onRemove }) => {  
-//   return (
-//     <ul>
-//       {contacts.map(item => (
-//         <ContactItem
-//           key={item.id}
-//           id={item.id}
-//           name={item.name}
-//           number={item.number}
-//           onRemove={onRemove}
-//         />
-//       ))}
-//     </ul>
-//   );
-// };
-
 const mapStateToProps = (state) => {
   return {
-    contacts: state.contacts,
+    contacts: state.contacts.contacts,
   };
 };
 
--- src/Components/PhoneBook/PhoneBook.js
+++ src/Components/PhoneBook/PhoneBook.js
@@ -17,49 +17,80 @@ class PhoneBook extends Component  {
     numberInputId = shortid.generate();
 
     handleChange = event => {
-        const { name, value } = event.currentTarget;
-        this.setState({
-            [name]:value
-        })        
+        this.setState({ [event.currentTarget.name]: event.currentTarget.value });  
     }    
     
-    handleSubmit = event => {
-        event.preventDefault();        
+    // handleSubmit = event => {
+    //     event.preventDefault();        
 
-        this.props.onSubmit(this.state);
-        this.reset();
-    }
-    reset = () => {
+    //     this.props.onSubmit(this.state);
+    //     this.reset();
+    // }
+    // reset = () => {
+    //     this.setState({
+    //         name: '',
+    //         number: ''
+    //     });
+    // }
+
+    // formSubmitHandler = ({name, number}) => {
+    //     const contact = {
+    //         id: shortid.generate(),
+    //         name,
+    //         number
+    //     };
+
+        
+    //     const inContacts = this.state.contacts.find(
+    //         item => item.name === contact.name,
+    //     );
+    //     if (inContacts !== undefined) {
+    //         alert(`${contact.name} is already in contacts`);        
+    //     return;
+    //     }        
+    //     this.setState(({ contacts }) => ({
+    //         contacts: [contact, ...contacts],
+    //     }));
+        
+    // }
+
+    formSubmitHandler = event => {
+        event.preventDefault()
+        this.props.onSubmit({ id: shortid.generate(), ...this.state });
         this.setState({
             name: '',
-            number: ''
-        });
+            number: '',
+    });
     }
 
-    formSubmitHandler = ({name, number}) => {
-        const contact = {
-            id: shortid.generate(),
-            name,
-            number
-        };
+    inContacts = (newName) => {
+        let pass = true;
 
-        const inContacts = this.state.contacts.find(
-            item => item.name === contact.name,
-        );
-        if (inContacts !== undefined) {
-            alert(`${contact.name} is already in contacts`);        
-        return;
-        } 
-        this.setState(({ contacts }) => ({
-            contacts: [contact, ...contacts],
-        }));     
+        this.state.contacts.forEach(({ name }) => {
+            if (name.toLowerCase() === newName.toLowerCase()) {
+                alert(`${name} is already in your contacts list`);
+                pass = false;
+            }
+        });            
+            return pass;
     }
-    
+
+    // inContacts = this.state.contacts.find(
+    //         item => item.name === contact.name,
+    //     );
+    //     if (inContacts !== undefined) {
+    //         alert(`${contact.name} is already in contacts`);        
+    //     return;
+    //     }        
+    //     this.setState(({ contacts }) => ({
+    //         contacts: [contact, ...contacts],
+    //     }));
+
         
     render() {        
         return (            
-                <form onSubmit={this.handleSubmit}  className="form">
-                   <label className={styles.label} htmlFor={this.nameInputId}>Name
+                <form onSubmit={this.formSubmitHandler} className="form">
+                   <label className={styles.label} htmlFor={this.nameInputId} >Name
                        <input
                         type="text"
                         name="name"
@@ -88,12 +119,14 @@ class PhoneBook extends Component  {
 
 const mapStateToProps = (state) => {
   return {
-    contacts: state.contacts,
+    contacts: state.contacts.contacts,
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-    onSubmit: ({ name, number }) => dispatch(contactsActions.addContact({ name, number }))
+    onSubmit: ({ id, name, number  }) => dispatch(contactsActions.addContact({ id, name, number }))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(PhoneBook);
+
+    
